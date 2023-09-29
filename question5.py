b74e105f69958f8b9a1dcdac8f9a5b22ac7d5689
garghar06 <145961408+garghar06@users.noreply.github.com>
2023-09-29 16:42:56 +0530

    Update question5.py

--- question5.py
+++ question5.py
@@ -4,12 +4,12 @@ with open("test.txt") as f:
     data = f.read()
 
 data = data.split('\n')
-print("data",data)
+
 x = [int(row.split(' ')[0]) for row in data]
-print("x",x)
+
 
 y = [int(row.split(' ')[1]) for row in data]
-print("y",y)
+
 plt.plot(x, y)
 
 plt.xlabel('x - axis')
